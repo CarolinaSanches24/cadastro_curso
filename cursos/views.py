1058df9294e6cbe23e611a21e85dde862f8e2242
CarolinaSanches24 <carolmosilva34@gmail.com>
2024-02-24 01:04:40 -0300

    feat:adicionando validacao de dados e otimizacao de cache

--- cursos/views.py
+++ cursos/views.py
@@ -1,8 +1,14 @@
 import django
 from django.shortcuts import render
 from cursos.forms import CursoForm
+from django.views.decorators.cache import cache_page
+from cursos.models import Curso # importar o modelo de curso
+
+
 # Create your views here.
+@cache_page(30) # TEMPO de espera de 30segundos
 def criar_curso(request):
+    cursos = Curso.objects.all()
     form = CursoForm(request.POST or None)
     sucesso = False
     if form.is_valid():
@@ -11,5 +17,7 @@ def criar_curso(request):
     contexto = {
         'form':form,
         'sucesso':sucesso,
+        'cursos':cursos,
     }
     return render(request,'criar_curso.html', contexto)
+
