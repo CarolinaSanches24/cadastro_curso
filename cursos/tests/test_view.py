97aa48a6c31bbd868fbf41c679804a0a2c8dba36
CarolinaSanches24 <carolmosilva34@gmail.com>
2024-03-04 18:24:00 -0300

    :sparkles: feat: adicionando teste unitario view

--- cursos/tests/test_view.py
+++ cursos/tests/test_view.py
@@ -1,5 +1,9 @@
 from pytest_django.asserts import assertTemplateUsed
+import pytest
 
-
-def test_curso_view_deve_retornar_template_criar_curso():
+@pytest.mark.django_db
+def test_curso_view_deve_retornar_template_criar_curso(client):
+    response = client.get('/curso/criar_curso/');
+    assert response.status_code == 200
+    assertTemplateUsed(response,'criar_curso.html')
     
